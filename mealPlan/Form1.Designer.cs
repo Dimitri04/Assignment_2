46ca035403b4ef65590eec3b529155d5bb945637
Dimitri04 <200382889@student.georgianc.on.ca>
2017-12-05 18:08:48 -0400

    Meal Plan Design

---
+++ mealPlan/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace mealPlan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.lblQunatity1 = new System.Windows.Forms.Label();
+            this.lblQunatity2 = new System.Windows.Forms.Label();
+            this.lblQunatity3 = new System.Windows.Forms.Label();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnOrderNow = new System.Windows.Forms.Button();
+            this.lblType1 = new System.Windows.Forms.Label();
+            this.lblType2 = new System.Windows.Forms.Label();
+            this.lblType3 = new System.Windows.Forms.Label();
+            this.lblQtyErr1 = new System.Windows.Forms.Label();
+            this.lblQtyErr2 = new System.Windows.Forms.Label();
+            this.lblQtyErr3 = new System.Windows.Forms.Label();
+            this.lblShowSubTotal = new System.Windows.Forms.Label();
+            this.lblShowTax = new System.Windows.Forms.Label();
+            this.lblShowTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Breakfast";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(96, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lunch";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(96, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dinner";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(309, 95);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(273, 33);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(309, 168);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(273, 33);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(309, 241);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(273, 33);
+            this.comboBox3.TabIndex = 5;
+            // 
+            // lblQunatity1
+            // 
+            this.lblQunatity1.AutoSize = true;
+            this.lblQunatity1.Location = new System.Drawing.Point(668, 98);
+            this.lblQunatity1.Name = "lblQunatity1";
+            this.lblQunatity1.Size = new System.Drawing.Size(92, 25);
+            this.lblQunatity1.TabIndex = 6;
+            this.lblQunatity1.Text = "Quantity";
+            // 
+            // lblQunatity2
+            // 
+            this.lblQunatity2.AutoSize = true;
+            this.lblQunatity2.Location = new System.Drawing.Point(668, 171);
+            this.lblQunatity2.Name = "lblQunatity2";
+            this.lblQunatity2.Size = new System.Drawing.Size(92, 25);
+            this.lblQunatity2.TabIndex = 7;
+            this.lblQunatity2.Text = "Qunatity";
+            // 
+            // lblQunatity3
+            // 
+            this.lblQunatity3.AutoSize = true;
+            this.lblQunatity3.Location = new System.Drawing.Point(668, 244);
+            this.lblQunatity3.Name = "lblQunatity3";
+            this.lblQunatity3.Size = new System.Drawing.Size(92, 25);
+            this.lblQunatity3.TabIndex = 8;
+            this.lblQunatity3.Text = "Qunatity";
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Location = new System.Drawing.Point(669, 383);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(91, 25);
+            this.lblSubtotal.TabIndex = 9;
+            this.lblSubtotal.Text = "Subtotal";
+            // 
+            // lblTax
+            // 
+            this.lblTax.AutoSize = true;
+            this.lblTax.Location = new System.Drawing.Point(668, 430);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(48, 25);
+            this.lblTax.TabIndex = 10;
+            this.lblTax.Text = "Tax";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(668, 477);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(60, 25);
+            this.lblTotal.TabIndex = 11;
+            this.lblTotal.Text = "Total";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(804, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(804, 168);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(804, 241);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 31);
+            this.textBox3.TabIndex = 14;
+            // 
+            // btnOrderNow
+            // 
+            this.btnOrderNow.Location = new System.Drawing.Point(417, 319);
+            this.btnOrderNow.Name = "btnOrderNow";
+            this.btnOrderNow.Size = new System.Drawing.Size(165, 46);
+            this.btnOrderNow.TabIndex = 15;
+            this.btnOrderNow.Text = "Order Now";
+            this.btnOrderNow.UseVisualStyleBackColor = true;
+            // 
+            // lblType1
+            // 
+            this.lblType1.AutoSize = true;
+            this.lblType1.Location = new System.Drawing.Point(225, 98);
+            this.lblType1.Name = "lblType1";
+            this.lblType1.Size = new System.Drawing.Size(60, 25);
+            this.lblType1.TabIndex = 16;
+            this.lblType1.Text = "Type";
+            // 
+            // lblType2
+            // 
+            this.lblType2.AutoSize = true;
+            this.lblType2.Location = new System.Drawing.Point(225, 171);
+            this.lblType2.Name = "lblType2";
+            this.lblType2.Size = new System.Drawing.Size(60, 25);
+            this.lblType2.TabIndex = 17;
+            this.lblType2.Text = "Type";
+            // 
+            // lblType3
+            // 
+            this.lblType3.AutoSize = true;
+            this.lblType3.Location = new System.Drawing.Point(225, 244);
+            this.lblType3.Name = "lblType3";
+            this.lblType3.Size = new System.Drawing.Size(60, 25);
+            this.lblType3.TabIndex = 18;
+            this.lblType3.Text = "Type";
+            // 
+            // lblQtyErr1
+            // 
+            this.lblQtyErr1.AutoSize = true;
+            this.lblQtyErr1.Location = new System.Drawing.Point(942, 98);
+            this.lblQtyErr1.Name = "lblQtyErr1";
+            this.lblQtyErr1.Size = new System.Drawing.Size(70, 25);
+            this.lblQtyErr1.TabIndex = 19;
+            this.lblQtyErr1.Text = "label4";
+            // 
+            // lblQtyErr2
+            // 
+            this.lblQtyErr2.AutoSize = true;
+            this.lblQtyErr2.Location = new System.Drawing.Point(942, 171);
+            this.lblQtyErr2.Name = "lblQtyErr2";
+            this.lblQtyErr2.Size = new System.Drawing.Size(70, 25);
+            this.lblQtyErr2.TabIndex = 20;
+            this.lblQtyErr2.Text = "label4";
+            // 
+            // lblQtyErr3
+            // 
+            this.lblQtyErr3.AutoSize = true;
+            this.lblQtyErr3.Location = new System.Drawing.Point(942, 244);
+            this.lblQtyErr3.Name = "lblQtyErr3";
+            this.lblQtyErr3.Size = new System.Drawing.Size(70, 25);
+            this.lblQtyErr3.TabIndex = 21;
+            this.lblQtyErr3.Text = "label4";
+            // 
+            // lblShowSubTotal
+            // 
+            this.lblShowSubTotal.AutoSize = true;
+            this.lblShowSubTotal.Location = new System.Drawing.Point(804, 383);
+            this.lblShowSubTotal.Name = "lblShowSubTotal";
+            this.lblShowSubTotal.Size = new System.Drawing.Size(70, 25);
+            this.lblShowSubTotal.TabIndex = 22;
+            this.lblShowSubTotal.Text = "label4";
+            // 
+            // lblShowTax
+            // 
+            this.lblShowTax.AutoSize = true;
+            this.lblShowTax.Location = new System.Drawing.Point(804, 430);
+            this.lblShowTax.Name = "lblShowTax";
+            this.lblShowTax.Size = new System.Drawing.Size(70, 25);
+            this.lblShowTax.TabIndex = 23;
+            this.lblShowTax.Text = "label5";
+            // 
+            // lblShowTotal
+            // 
+            this.lblShowTotal.AutoSize = true;
+            this.lblShowTotal.Location = new System.Drawing.Point(804, 477);
+            this.lblShowTotal.Name = "lblShowTotal";
+            this.lblShowTotal.Size = new System.Drawing.Size(70, 25);
+            this.lblShowTotal.TabIndex = 24;
+            this.lblShowTotal.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1449, 594);
+            this.Controls.Add(this.lblShowTotal);
+            this.Controls.Add(this.lblShowTax);
+            this.Controls.Add(this.lblShowSubTotal);
+            this.Controls.Add(this.lblQtyErr3);
+            this.Controls.Add(this.lblQtyErr2);
+            this.Controls.Add(this.lblQtyErr1);
+            this.Controls.Add(this.lblType3);
+            this.Controls.Add(this.lblType2);
+            this.Controls.Add(this.lblType1);
+            this.Controls.Add(this.btnOrderNow);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblTax);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.lblQunatity3);
+            this.Controls.Add(this.lblQunatity2);
+            this.Controls.Add(this.lblQunatity1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label lblQunatity1;
+        private System.Windows.Forms.Label lblQunatity2;
+        private System.Windows.Forms.Label lblQunatity3;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label lblTax;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnOrderNow;
+        private System.Windows.Forms.Label lblType1;
+        private System.Windows.Forms.Label lblType2;
+        private System.Windows.Forms.Label lblType3;
+        private System.Windows.Forms.Label lblQtyErr1;
+        private System.Windows.Forms.Label lblQtyErr2;
+        private System.Windows.Forms.Label lblQtyErr3;
+        private System.Windows.Forms.Label lblShowSubTotal;
+        private System.Windows.Forms.Label lblShowTax;
+        private System.Windows.Forms.Label lblShowTotal;
+    }
+}
+
